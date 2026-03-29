2559065af556354634c64aaf6104f5a24bc13fde
IagoRochaDev <iagor.oliveira@gmail.com>
2026-03-29 19:13:46 -0300

    feat(di): provide Firestore and RemoteDataSource and update TransactionRepository dependencies

--- app/src/main/java/com/devrochaiago/expenseapp/di/AppModule.kt
+++ app/src/main/java/com/devrochaiago/expenseapp/di/AppModule.kt
@@ -4,8 +4,14 @@ import android.app.Application
 import androidx.room.Room
 import com.devrochaiago.expenseapp.data.local.ExpenseDatabase
 import com.devrochaiago.expenseapp.data.local.dao.TransactionDao
+import com.devrochaiago.expenseapp.data.remote.TransactionRemoteDataSource
+import com.devrochaiago.expenseapp.data.remote.TransactionRemoteDataSourceImpl
 import com.devrochaiago.expenseapp.data.repository.TransactionRepositoryImpl
+import com.devrochaiago.expenseapp.domain.repository.AuthRepository
 import com.devrochaiago.expenseapp.domain.repository.TransactionRepository
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -34,7 +40,25 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideTransactionRepository(dao: TransactionDao): TransactionRepository {
-        return TransactionRepositoryImpl(dao)
+    fun provideTransactionRepository(
+        dao: TransactionDao,
+        remoteDataSource: TransactionRemoteDataSource,
+        authRepository: AuthRepository
+    ): TransactionRepository {
+        return TransactionRepositoryImpl(dao, remoteDataSource, authRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirestore(): FirebaseFirestore {
+        return Firebase.firestore
+    }
+
+    @Provides
+    @Singleton
+    fun provideTransactionRemoteDataSource(
+        firestore: FirebaseFirestore
+    ): TransactionRemoteDataSource {
+        return TransactionRemoteDataSourceImpl(firestore)
     }
 }
